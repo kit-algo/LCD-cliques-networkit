a49e5a9759a16576c67ccc4d70e4d6fc03d9b195
Michael Hamann <michael.hamann@kit.edu>
2017-06-20 15:45:44 +0200

    SCD: add helper for combining two SCD algorithms
    This allows to combine two SCD algorithms such that first the first
    algorithm is executed on the given seed(s) and then the result of
    the first algorithm is used as the seed set of the second algorithm.
    This is particularly useful for seeding an algorithm with a clique.

---
+++ networkit/cpp/scd/CombinedSCD.cpp
@@ -0,0 +1,17 @@
+#include <networkit/scd/CombinedSCD.hpp>
+
+namespace NetworKit {
+
+CombinedSCD::CombinedSCD(const Graph &G, SelectiveCommunityDetector &first,
+                         SelectiveCommunityDetector &second)
+    : SelectiveCommunityDetector(G), first(first), second(second) {}
+
+std::set<node> CombinedSCD::expandOneCommunity(node s) {
+    return second.expandOneCommunity(first.expandOneCommunity(s));
+}
+
+std::set<node> CombinedSCD::expandOneCommunity(const std::set<node> &s) {
+    return second.expandOneCommunity(first.expandOneCommunity(s));
+}
+
+} // namespace NetworKit
