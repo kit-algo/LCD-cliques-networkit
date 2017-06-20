38dd4585a656b9048361bb2ca00f355b5ad46af7
Michael Hamann <michael.hamann@kit.edu>
2017-06-20 15:36:33 +0200

    Add utility for the incremental uniformly random selection
    This class can be used when a selection among a number of equal elements
    needs to be made. The class does not assume that the elements are all known
    beforehand but that elements are discovered one after another.

---
+++ networkit/cpp/auxiliary/UniformRandomSelector.h
@@ -0,0 +1,54 @@
+#ifndef UNIFORMRANDOMSELECTOR_H
+#define UNIFORMRANDOMSELECTOR_H
+
+#include "Random.h"
+
+#include <cstddef>
+
+namespace Aux {
+
+/**
+ * Select uniformly at random from a set of elements
+ * that is discovered incrementally. Every time you
+ * discover a new element, you tell the selector and
+ * it will tell if you should keep the new element or
+ * your old element. As more and more elements are
+ * discovered, it will be less and less likely that
+ * the new element is chosen in order to ensure that
+ * the selection is uniformly at random. The process
+ * can be reset when a new class of elements is found
+ * that shall be used instead.
+ */
+class UniformRandomSelector {
+public:
+	/**
+	 * Initialize the random select for one element.
+	 */
+	UniformRandomSelector() : counter(1) {};
+
+	/**
+	 * Add the next element.
+	 *
+	 * Note that this must not be called for the first element.
+	 *
+	 * @return If the new element shall be accepted/kept.
+	 */
+	bool add_element() {
+		++counter;
+		return (Random::real() < 1.0 / counter);
+	}
+
+	/**
+	 * Reset the process to one element again.
+	 */
+	void reset() {
+		counter = 1;
+	}
+private:
+	size_t counter;
+};
+
+} // namespace Aux
+
+
+#endif // UNIFORMRANDOMSELECTOR_H
